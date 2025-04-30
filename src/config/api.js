4b87d264312ef8c0bc565fcd3df192b66be25970
aikan86 <giuliopolent@gmail.com>
2025-04-30 17:08:42 +0200

    fix api

--- src/config/api.js
+++ src/config/api.js
@@ -3,7 +3,7 @@ const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:1337';
 export default {
   baseURL: API_URL,
   endpoints: {
-    events: '/api/events',
+    events: '/api/eventis',
     categories: '/api/categories',
     users: '/api/users',
   }
